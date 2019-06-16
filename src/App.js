a3a7f584bec7e142fad5c8aa344e93b3db1ecf3e
Zevi Liao <jointjm@gmail.com>
2019-06-16 13:31:59 +0800

    init proj

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react'
 import logo from './logo.svg';
-import './App.css';
+import { instanceOf } from 'prop-types';
+import { withCookies, Cookies } from 'react-cookie';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+
+class App extends Component {
+  static propTypes = {
+    cookies: instanceOf(Cookies).isRequired
+  };
+
+  constructor(props) {
+    super(props);
+
+    const { cookies } = props;
+    this.state = {
+      themeName: cookies.get('themeName') || 'A'
+    };
+    require('./theme/theme' + this.state.themeName + '/css/index.css')
+    require('./theme/theme' + this.state.themeName + '/css/App.css')
+  }
+
+  handleThemeNameChange = (themeName) =>{
+    const { cookies } = this.props;
+    cookies.set('themeName', themeName, { path: '/' });
+    this.setState({ themeName });
+    window.location.reload();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <section>
+            <button onClick={() => this.handleThemeNameChange('A')}>Theme A</button>
+            <button onClick={() => this.handleThemeNameChange('B')}>Theme B</button>
+          </section>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
-  );
+        </header>
+
+      </div>
+    )
+  }
 }
 
-export default App;
+export default withCookies(App)
